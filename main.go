67cb995be56a474f10728d5a368d8dadca6a6bfc
wechiro <wesley.rodrigues@email.com>
2026-04-28 22:02:30 -0300

    Calculadora CLI em GO

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+var num1 float64
+var num2 float64
+var operacao string
+
+func soma(a, b float64) float64 {
+	return a + b
+}
+
+func subtracao(a, b float64) float64 {
+	return a - b
+}
+
+func multiplicacao(a, b float64) float64 {
+	return a * b
+}
+
+func main() {
+
+	fmt.Println("=== CALCULADORA BÁSICA ===")
+	fmt.Println("Operaçãoes: +, - e *. Digite 'sair' para encerrar.")
+
+	for {
+		fmt.Print("\nOperação: ")
+		fmt.Scan(&operacao)
+
+		if operacao == "sair" {
+			fmt.Println("Encerrando...até mais!")
+			break
+		}
+
+		fmt.Print("Primeiro número: ")
+		fmt.Scan(&num1)
+		fmt.Print("Segundo número: ")
+		fmt.Scan(&num2)
+
+		if operacao == "+" {
+			fmt.Println("Resultado: \n", soma(num1, num2))
+		} else if operacao == "-" {
+			fmt.Println("Resultado: \n", subtracao(num1, num2))
+		} else if operacao == "*" {
+			fmt.Println("Resultado: \n", multiplicacao(num1, num2))
+		} else {
+			fmt.Println("Operacao Invalida.")
+		}
+
+	}
+}
